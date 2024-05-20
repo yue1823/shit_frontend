357a85b5e9590c09e0e442dfa8c1444af667c3c4
yue <112491532+yue1823@users.noreply.github.com>
2024-05-20 06:12:12 +0000

    test

---
+++ build/src/example.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Input } from "antd";
+import "./src/example.css";
+function Example() {
+  return;
+  <div>
+    <Input></Input>
+  </div>;
+}
+
+export default Example;
